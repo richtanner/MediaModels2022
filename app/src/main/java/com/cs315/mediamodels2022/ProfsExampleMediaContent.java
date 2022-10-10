91b600234cb958109bf67be4ffdb360ce0536cff
Despacita21 <lan20a@acu.edu>
2022-10-10 16:25:47 -0500

    Initial Commit - Small Change

--- app/src/main/java/com/cs315/mediamodels2022/ProfsExampleMediaContent.java
+++ app/src/main/java/com/cs315/mediamodels2022/ProfsExampleMediaContent.java
@@ -24,13 +24,13 @@ public class ProfsExampleMediaContent {
     // CS315: DO THIS
     // TODO: COPY this class to create your OWN MediaContent.  Then, create five NEW media objects here. Complete with images and URLs. DELETE the two existing movies, they are only here as an example
 
-    private static final String movie1Title = "Generic Rom Com:";
+    private static final String movie1Title = "Generic Rom Com: ";
     private static final String movie1Description = "Naturally, focuses on 2 people who really shouldn’t be together. It somehow becomes mutually advantageous for them to be together, or perhaps, they are interested in each other but their romance is based on false pretenses. Because the leads generally have no charisma, they each need a zany best friend who inserts dirty jokes wherever necessary. The two leads will fall in love in an extended music montage set to the music of Sixpence None the Richer. Once they find out about the false pretenses, they will take some time apart but realize how much they care for the other. Then comes “The Chase,” which can either be in an airport (not legal anymore), in traffic or somehow on boats. \n \nYou've seen this before, but not with these two actors!";
     private static final String movie1Year = "2015";
     private static final String movie1Image = "genericromcom";
     private static final String movie1Weblink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
 
-    private static final String actionTitle = "Every Action Movie Ever";
+    private static final String actionTitle = "Every Cartoon Movie Ever";
     private static final String actionDescription = "The villain has left the hero for dead, or killed the hero’s brother, sister, parents, wife or family pets. Filled with righteous fury, the hero tools up and embarks on a bloody rampage.  \n \nIf the movie does well, we will do it again in the sequel! \n \nMaybe the villain tries to get revenge on the hero for getting revenge on the villain in this movie...";
     private static final String actionYear = "2017";
     private static final String actionImage = "genericaction";
