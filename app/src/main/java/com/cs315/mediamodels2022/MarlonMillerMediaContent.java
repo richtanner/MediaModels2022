6fda4d022f60c3fea6387e546a2e98b6964a2b79
Marlon Miller <mmillermatute18@gmail.com>
2022-10-11 08:14:23 -0500

    Strings from String.xml called from MarlonMillerMediaContent

--- app/src/main/java/com/cs315/mediamodels2022/MarlonMillerMediaContent.java
+++ app/src/main/java/com/cs315/mediamodels2022/MarlonMillerMediaContent.java
@@ -22,67 +22,67 @@ public class MarlonMillerMediaContent {
     // application crash. So I decided to use the simple strings as long as it runs.
     // *****************************************************************************************
 
-//    static Context context = myAppContext.getContext();
-//    static Resources resources = context.getResources();
-//    private static final String character1Name = resources.getString(R.string.AOTCharacter1Name);
-//    private static final String character1Description = resources.getString(R.string.AOTCharacter1Description);
-//    private static final String character1Year = resources.getString(R.string.AOTCharacter1Year);
-//    private static final String character1Image = String.valueOf(R.drawable.eren_jaegar);
-//    private static final String character1WebLink = resources.getString(R.string.AOTCharacter1WebLink);
+    static Context context = myAppContext.getContext();
+    static Resources resources = context.getResources();
+    private static final String character1Name = resources.getString(R.string.AOTCharacter1Name);
+    private static final String character1Description = resources.getString(R.string.AOTCharacter1Description);
+    private static final String character1Year = resources.getString(R.string.AOTCharacter1Year);
+    private static final String character1Image = String.valueOf(R.drawable.eren_jaegar);
+    private static final String character1WebLink = resources.getString(R.string.AOTCharacter1WebLink);
+
+    private static final String character2Name = resources.getString(R.string.AOTCharacter2Name);
+    private static final String character2Description = resources.getString(R.string.AOTCharacter2Description);
+    private static final String character2Year = resources.getString(R.string.AOTCharacter2Year);
+    private static final String character2Image = String.valueOf(R.drawable.reiner_braun);
+    private static final String character2WebLink = resources.getString(R.string.AOTCharacter2WebLink);
+
+    private static final String character3Name = resources.getString(R.string.AOTCharacter3Name);
+    private static final String character3Description = resources.getString(R.string.AOTCharacter3Description);
+    private static final String character3Year = resources.getString(R.string.AOTCharacter3Year);
+    private static final String character3Image = String.valueOf(R.drawable.armin);
+    private static final String character3WebLink = resources.getString(R.string.AOTCharacter3WebLink);
+
+    private static final String character4Name = resources.getString(R.string.AOTCharacter4Name);
+    private static final String character4Description = resources.getString(R.string.AOTCharacter4Description);
+    private static final String character4Year = resources.getString(R.string.AOTCharacter4Year);
+    private static final String character4Image = String.valueOf(R.drawable.mikasa);
+    private static final String character4WebLink = resources.getString(R.string.AOTCharacter4WebLink);
+
+    private static final String character5Name = resources.getString(R.string.AOTCharacter5Name);
+    private static final String character5Description = resources.getString(R.string.AOTCharacter5Description);
+    private static final String character5Year = resources.getString(R.string.AOTCharacter5Year);
+    private static final String character5Image = String.valueOf(R.drawable.floch);
+    private static final String character5WebLink = resources.getString(R.string.AOTCharacter5WebLink);
+
+//    private static final String character1Name = "Eren Jaegar";
+//    private static final String character1Description = "The boy who sought Freedom. Eren Jaegar is the main protagonist and antagonist of Attack on Titan. He is the last possessor of the Attack Titan, Warhammer Titan and Founding Titan, fighting for Eldia's freedom.";
+//    private static final String character1Year = "835";
+//    private static final String character1Image = "eren_jaegar";
+//    private static final String character1WebLink = "https://attackontitan.fandom.com/wiki/Eren_Jaeger_(Anime)";
 //
-//    private static final String character2Name = resources.getString(R.string.AOTCharacter2Name);
-//    private static final String character2Description = resources.getString(R.string.AOTCharacter2Description);
-//    private static final String character2Year = resources.getString(R.string.AOTCharacter2Year);
-//    private static final String character2Image = String.valueOf(R.drawable.reiner_braun);
-//    private static final String character2WebLink = resources.getString(R.string.AOTCharacter2WebLink);
+//    private static final String character2Name = "Reiner Braun";
+//    private static final String character2Description = "The Armoured Titan. Marley's Shield. The Solder/Warrior that deserved the best of both worlds.";
+//    private static final String character2Year = "830";
+//    private static final String character2Image = "reiner_braun";
+//    private static final String character2WebLink = "https://attackontitan.fandom.com/wiki/Reiner_Braun";
 //
-//    private static final String character3Name = resources.getString(R.string.AOTCharacter3Name);
-//    private static final String character3Description = resources.getString(R.string.AOTCharacter3Description);
-//    private static final String character3Year = resources.getString(R.string.AOTCharacter3Year);
-//    private static final String character3Image = String.valueOf(R.drawable.armin);
-//    private static final String character3WebLink = resources.getString(R.string.AOTCharacter3WebLink);
+//    private static final String character3Name = "Armin Arlert";
+//    private static final String character3Description = "The Dreamer and Thinker. The Eldian who killed Eren Jaegar, the Attack Titan.";
+//    private static final String character3Year = "834";
+//    private static final String character3Image = "armin";
+//    private static final String character3WebLink = "https://attackontitan.fandom.com/wiki/Armin_Arlert";
 //
-//    private static final String character4Name = resources.getString(R.string.AOTCharacter4Name);
-//    private static final String character4Description = resources.getString(R.string.AOTCharacter4Description);
-//    private static final String character4Year = resources.getString(R.string.AOTCharacter4Year);
-//    private static final String character4Image = String.valueOf(R.drawable.mikasa);
-//    private static final String character4WebLink = resources.getString(R.string.AOTCharacter4WebLink);
+//    private static final String character4Name = "Mikasa Ackerman";
+//    private static final String character4Description = "Strongest member of the 104th Survey Corps, and Eldian savior of humanity.";
+//    private static final String character4Year = "835";
+//    private static final String character4Image = "mikasa";
+//    private static final String character4WebLink = "https://attackontitan.fandom.com/wiki/Mikasa_Ackerman";
 //
-//    private static final String character5Name = resources.getString(R.string.AOTCharacter5Name);
-//    private static final String character5Description = resources.getString(R.string.AOTCharacter5Description);
-//    private static final String character5Year = resources.getString(R.string.AOTCharacter5Year);
-//    private static final String character5Image = String.valueOf(R.drawable.floch);
-//    private static final String character5WebLink = resources.getString(R.string.AOTCharacter5WebLink);
-
-    private static final String character1Name = "Eren Jaegar";
-    private static final String character1Description = "The boy who sought Freedom. Eren Jaegar is the main protagonist and antagonist of Attack on Titan. He is the last possessor of the Attack Titan, Warhammer Titan and Founding Titan, fighting for Eldia's freedom.";
-    private static final String character1Year = "835";
-    private static final String character1Image = "eren_jaegar";
-    private static final String character1WebLink = "https://attackontitan.fandom.com/wiki/Eren_Jaeger_(Anime)";
-
-    private static final String character2Name = "Reiner Braun";
-    private static final String character2Description = "The Armoured Titan. Marley's Shield. The Solder/Warrior that deserved the best of both worlds.";
-    private static final String character2Year = "830";
-    private static final String character2Image = "reiner_braun";
-    private static final String character2WebLink = "https://attackontitan.fandom.com/wiki/Reiner_Braun";
-
-    private static final String character3Name = "Armin Arlert";
-    private static final String character3Description = "The Dreamer and Thinker. The Eldian who killed Eren Jaegar, the Attack Titan.";
-    private static final String character3Year = "834";
-    private static final String character3Image = "armin";
-    private static final String character3WebLink = "https://attackontitan.fandom.com/wiki/Armin_Arlert";
-
-    private static final String character4Name = "Mikasa Ackerman";
-    private static final String character4Description = "Strongest member of the 104th Survey Corps, and Eldian savior of humanity.";
-    private static final String character4Year = "835";
-    private static final String character4Image = "mikasa";
-    private static final String character4WebLink = "https://attackontitan.fandom.com/wiki/Mikasa_Ackerman";
-
-    private static final String character5Name = "Floch Forster";
-    private static final String character5Description = "The Devil's Assistant. Floch is the second right hand man of Eren Jaegar and the leader to the new Eldian Empire and the Jaegarists";
-    private static final String character5Year = "835";
-    private static final String character5Image = "floch";
-    private static final String character5WebLink = "https://attackontitan.fandom.com/wiki/Floch_Forster";
+//    private static final String character5Name = "Floch Forster";
+//    private static final String character5Description = "The Devil's Assistant. Floch is the second right hand man of Eren Jaegar and the leader to the new Eldian Empire and the Jaegarists";
+//    private static final String character5Year = "835";
+//    private static final String character5Image = "floch";
+//    private static final String character5WebLink = "https://attackontitan.fandom.com/wiki/Floch_Forster";
 
     public List<MediaModel> createAOTCharacters () {
 
