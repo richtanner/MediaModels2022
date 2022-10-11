0f7cf94dca7812c5f21ac21713de5b407818fec2
Marlon Miller <mmillermatute18@gmail.com>
2022-10-10 22:19:55 -0500

    Initial Branch Creation Commit Message

---
+++ app/src/main/java/com/cs315/mediamodels2022/MarlonMillerMediaContent.java
@@ -0,0 +1,43 @@
+package com.cs315.mediamodels2022;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MarlonMillerMediaContent {
+
+    public static final Map<String,MediaModel> map = new HashMap<String,MediaModel>();
+    public static final List<MediaModel> AOT = new ArrayList<MediaModel>(); // Attack on Titan Characters
+
+    private static final String character1Name = "Eren Jaegar";
+    private static final String character1Description = "The boy who sought Freedom. Eren Jaegar is the main protagonist and antagonist of Attack on Titan. He is the last possessor of the Attack Titan, Warhammer Titan and Founding Titan, fighting for Eldia's freedom.";
+    private static final String character1Year = "835";
+    private static final String character1Image = "eren_jaegar";
+    private static final String character1WebLink = "https://attackontitan.fandom.com/wiki/Eren_Jaeger_(Anime)";
+
+    private static final String character2Name = "Reiner Braun";
+    private static final String character2Description = "The Armoured Titan. Marley's Shield. The Solder/Warrior that deserved the best of both worlds.";
+    private static final String character2Year = "830";
+    private static final String character2Image = "reiner_braun";
+    private static final String character2WebLink = "https://attackontitan.fandom.com/wiki/Reiner_Braun";
+
+    private static final String character3Name = "Armin Arlert";
+    private static final String character3Description = "The Dreamer and Thinker. The Eldian who killed Eren Jaegar, the Attack Titan.";
+    private static final String character3Year = "834";
+    private static final String character3Image = "armin";
+    private static final String character3WebLink = "https://attackontitan.fandom.com/wiki/Armin_Arlert";
+
+    private static final String character4Name = "Mikasa Ackerman";
+    private static final String character4Description = "Strongest member of the 104th Survey Corps, and Eldian savior of humanity.";
+    private static final String character4Year = "835";
+    private static final String character4Image = "mikasa";
+    private static final String character4WebLink = "https://attackontitan.fandom.com/wiki/Mikasa_Ackerman";
+
+    private static final String character5Name = "Floch Forster";
+    private static final String character5Description = "The Devil's Assistant. Floch is the second right hand man of Eren Jaegar and the leader to the new Eldian Empire and the Jaegarists";
+    private static final String character5Year = "835";
+    private static final String character5Image = "floch";
+    private static final String character5WebLink = "https://attackontitan.fandom.com/wiki/Floch_Forster";
+
+}
