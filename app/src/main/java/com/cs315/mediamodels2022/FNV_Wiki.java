e2b9c49238ed99b2bb02b42bd74c39b98cd8e169
Dieharts <384317Rageur>
2022-10-10 20:58:59 -0500

    Debugging the app to make it work, and fixing an issue with the pictures not showing up (setting image_1 to name of the picture)

---
+++ app/src/main/java/com/cs315/mediamodels2022/FNV_Wiki.java
@@ -0,0 +1,19 @@
+package com.cs315.mediamodels2022;
+
+
+import android.app.Application;
+import android.content.Context;
+
+public class FNV_Wiki extends Application {
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
